2214abf2abfa9c876e748db99e6068ef4af73937
Hongyuan-Z0716 <hongyzhang@clarku.edu>
2026-02-06 18:57:11 -0500

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Intelligent Recommendation System</h1>
+      <p>Welcome to the recommendation platform.</p>
+    </div>
+  );
+}
+
+export default App;
